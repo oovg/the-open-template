fd5f8c0a9b6a34b7b90d2d14a651b08906373e11
oovg <vengist@odyssy.io>
2023-08-29 12:30:49 -0700

    adds markdown styles

--- src/components/SurveysPost.tsx
+++ src/components/SurveysPost.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Box, Heading } from '@chakra-ui/react'
 import ReactMarkdown from 'react-markdown'
+import style from './markdown-styles.module.css';
 
 type Props = {
   content: string
@@ -12,13 +13,12 @@ type Props = {
 
 export const SurveysPost = ({ content, title, slug }: Props) => (
 
-    <Box className="max-w-2xl mx-auto" mb={12}>
+    <Box maxW="2xl" mx="auto" mb={12}>
       <Heading mb={6}>{title}</Heading>
-      <Box
-      >
-      <ReactMarkdown>
-      {content}
-      </ReactMarkdown>
+      <Box>
+        <ReactMarkdown className={style.markdown}>
+          {content}
+        </ReactMarkdown>
       </Box>
     </Box>
 )
