ab85b521692af8b566d8a257645e263c6ec17503
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-09-02 12:27:31 -0700

    patch Section.tsx
    auto-linted

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,35 +1,148 @@
 import React from 'react'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Image, Text } from '@chakra-ui/react'
 import { Rule } from '@/components'
 
 export const Section = () => (
   <Box w="100%">
     <Box maxW="720px" mx="auto" px={12} py={12}>
-        <Rule />
-        <Box>
-            <Text textStyle="paragraph">It’s an open secret, a scandalous bit of folk knowledge known among realists and parriahs alike, that the ones who seek totalizing systems are the ones most excluded from the touch of completion. A preliminary definition of truth for the modern age: that which an institution cannot see. One needs the vision of the outside, a place not so timely, to see that  the project of administration, the distance of the template, is   blind to the singularities - the traces of difference and happenstance which contain in their roughness the strange wholeness of the world.</Text>
-            <Text textStyle="paragraph">Concepts are a different story. They may appear to serve administration and ownership in little mercenary episodes, but they are always slipping toward free encounter and discovery, the trickster germ of the untimely. When Von Neumann and Wiener developed the field of cybernetics, they couldn’t know that they were opening up a line of inquiry that would expose them, in their bids at control, to the open time  of the earth. Bateson’s information ecology: everywhere there is entanglement, recursion, the categorically unstable reflexivity of network systems, <i>there is the earth</i>: an unstoppable difference, burrowing unstable paths that revive the dead, deliver the unborn, an open machine for the production of new temporalities.</Text>
-            <Text textStyle="paragraph">Programming culture tells the same story, though perhaps on a different axis - the earthen silica of the computer leaping out of the bound to give emergence its voice. The open source movement discovered, in following the unruly openings of turing completion, the <i>bazaar</i>, a terrain too vast to contain (or to own). The flat ontology of engineering, where networks move faster than hierarchies and nothing is sacred. It was a wisdom gained by work, beyond all ideological discipline, beyond abstraction and theology, and because of that, it necessarily pointed past the institution.</Text>
-            <Text textStyle="paragraph">Cryptography enabled that intuition, allowing it to be realized as an infrastructure. What are the local minima of institutional constructs? What anarchistic stop-gaps across time and space might nonetheless enable legibility in what Moten and Turing both called <i>The Universal Machine</i>? The builders of web3 were a recursive public: the product of their work embodied the protocols of their own development. They built in ambulant guilds of free association, an ethic of encounter developed solely to expose each node to that machine - the non-institutional charge, emergence, the rough wholeness. In the infrastructure of the cryptographic ledger is the promise of a society of friends - free encounter among entities under non-coercive conditions, for the earth is magnetically repelled by the atmosphere of coercion. Supremacy is a dead ecology.</Text>
-             <Text textStyle="paragraph"><img
-        src="../assets/ecology.png"
-      />
-</Text>
-          <Text textStyle="paragraph">Ruskin <a href="http://homes.ieu.edu.tr/arch204/READINGS/02_RUSKIN.pdf" target="_blank" rel="noopener noreferrer">writes</a> in 1853 of the tradition of the gothic masons, journeyman practitioners whose art contained the generic traits of free association and encounter with the earth (“in gothic work," he says, "the workman must have been altogether set free”). The traits are as follows: <b>Savageness or Rudeness, Love of Change, Love of Nature, Disturbed Imagination, Obstinacy</b>, and  <b>Generosity</b>. The brash artistry had no time for ceremony and manicured likenesses, generating “creations of ungainly shape and rigid limb, but full of wolfish life.” The successive builds made for patchwork styles that embraced changed; the spires and buttresses charged with an obstinance and rigidity “not merely stable, but active … the peculiar energy which gives tension to movement, and stiffness to resistance, which makes the fiercest lightning forked rather than curved.”</Text>
-        </Box>
-        <Box>
-            <Text textStyle="paragraph">In the ethos of free work is a negotiation, a pact where the audacity of liberty is checked by the patience of emergence. The journeyman makes acquaintance with the networked earth. “The love of change which becomes morbid and feverish in following the haste of the hunter and the rage of the combatant, is at once soothed and satisfied as it watches the wandering of the tendril, and the budding of the flower. … The sculptor who sought for his models among the forest leaves, could not but quickly and deeply feel that complexity need not involve the loss of grace, nor richness that of repose.” The gothic labors are distinct for this naturalism, this realism, built out of “a profound sympathy with the fulness and wealth of the material universe.”</Text>
-            <Text textStyle="paragraph">In the memelore and production of the builders of the decentralized web, we find these same attributes. We find the savage grasp of materiality - yes, even the materiality of transistor charges and logic gates - loved and embraced for all its robustness. When the relationship to the elements isn’t alienated by the distracted and antireal metaphysics of hierarchy, that abundance unfolds, so that the intention of the producer is fused with the intrigue and chance of the material instance. That’s the question- how can we bring the material instance to bear in building, conceptualizing, and manifesting the flamboyantly fractal and utterly irreverent scale invariance of the network, the (anti)thing that touches and ignites the rough whole? In work is an imaginatively sober play.</Text>
-            <Text textStyle="paragraph">Borrowing from the ethos of the builders, we announce The Open Machine, a publication press for the advancement of concepts adequate to the network. We forego authority and professionalism for nonlinearity and free association. We call to task interdisciplinary and transhistorical inputs for the sake of conceptual pandemonium. We study the rhythms and arrows of meme propagation so as to venture farther out onto limbs of recursion and emergence. We work through the abandonment issues of those that have forsaken the lords of enclosure and find new idols, machine elves and mathematical witches of the open past and the long future.</Text>
-            <Text textStyle="paragraph">We create in an atmosphere of savageness and generosity. We view the expanse with the disturbed eye of reality. Atop the flat bazaar, we build an untimely cathedral, a mayhem of free successions that might iterate off into the outer spaces, legibility in non-institutional time.</Text>
-              </Box>
-        <Box>
-          <Text textStyle="paragraph"><img
-        src="../assets/cathedral void.png"
-      />
-</Text>
-        </Box> 
-        <Rule />
+      <Rule />
+      <Box>
+        <Text textStyle="paragraph">
+          It&apos;s an open secret, a scandalous bit of folk knowledge known
+          among realists and parriahs alike, that the ones who seek totalizing
+          systems are the ones most excluded from the touch of completion. A
+          preliminary definition of truth for the modern age: that which an
+          institution cannot see. One needs the vision of the outside, a place
+          not so timely, to see that the project of administration, the distance
+          of the template, is blind to the singularities - the traces of
+          difference and happenstance which contain in their roughness the
+          strange wholeness of the world.
+        </Text>
+        <Text textStyle="paragraph">
+          Concepts are a different story. They may appear to serve
+          administration and ownership in little mercenary episodes, but they
+          are always slipping toward free encounter and discovery, the trickster
+          germ of the untimely. When Von Neumann and Wiener developed the field
+          of cybernetics, they couldn&apos;t know that they were opening up a
+          line of inquiry that would expose them, in their bids at control, to
+          the open time of the earth. Bateson&apos;s information ecology:
+          everywhere there is entanglement, recursion, the categorically
+          unstable reflexivity of network systems, <i>there is the earth</i>: an
+          unstoppable difference, burrowing unstable paths that revive the dead,
+          deliver the unborn, an open machine for the production of new
+          temporalities.
+        </Text>
+        <Text textStyle="paragraph">
+          Programming culture tells the same story, though perhaps on a
+          different axis - the earthen silica of the computer leaping out of the
+          bound to give emergence its voice. The open source movement
+          discovered, in following the unruly openings of turing completion, the{' '}
+          <i>bazaar</i>, a terrain too vast to contain (or to own). The flat
+          ontology of engineering, where networks move faster than hierarchies
+          and nothing is sacred. It was a wisdom gained by work, beyond all
+          ideological discipline, beyond abstraction and theology, and because
+          of that, it necessarily pointed past the institution.
+        </Text>
+        <Text textStyle="paragraph">
+          Cryptography enabled that intuition, allowing it to be realized as an
+          infrastructure. What are the local minima of institutional constructs?
+          What anarchistic stop-gaps across time and space might nonetheless
+          enable legibility in what Moten and Turing both called{' '}
+          <i>The Universal Machine</i>? The builders of web3 were a recursive
+          public: the product of their work embodied the protocols of their own
+          development. They built in ambulant guilds of free association, an
+          ethic of encounter developed solely to expose each node to that
+          machine - the non-institutional charge, emergence, the rough
+          wholeness. In the infrastructure of the cryptographic ledger is the
+          promise of a society of friends - free encounter among entities under
+          non-coercive conditions, for the earth is magnetically repelled by the
+          atmosphere of coercion. Supremacy is a dead ecology.
+        </Text>
+        <Text textStyle="paragraph">
+          <Image src="../assets/ecology.png" alt="ecology" />
+        </Text>
+        <Text textStyle="paragraph">
+          Ruskin{' '}
+          <a
+            href="http://homes.ieu.edu.tr/arch204/READINGS/02_RUSKIN.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            writes
+          </a>{' '}
+          in 1853 of the tradition of the gothic masons, journeyman
+          practitioners whose art contained the generic traits of free
+          association and encounter with the earth (&ldquo;in gothic
+          work,&rdquo; he says, &ldquo;the workman must have been altogether set
+          free&rdquo;). The traits are as follows:{' '}
+          <b>
+            Savageness or Rudeness, Love of Change, Love of Nature, Disturbed
+            Imagination, Obstinacy
+          </b>
+          , and <b>Generosity</b>. The brash artistry had no time for ceremony
+          and manicured likenesses, generating “creations of ungainly shape and
+          rigid limb, but full of wolfish life.” The successive builds made for
+          patchwork styles that embraced changed; the spires and buttresses
+          charged with an obstinance and rigidity “not merely stable, but active
+          … the peculiar energy which gives tension to movement, and stiffness
+          to resistance, which makes the fiercest lightning forked rather than
+          curved.”
+        </Text>
+      </Box>
+      <Box>
+        <Text textStyle="paragraph">
+          In the ethos of free work is a negotiation, a pact where the audacity
+          of liberty is checked by the patience of emergence. The journeyman
+          makes acquaintance with the networked earth. “The love of change which
+          becomes morbid and feverish in following the haste of the hunter and
+          the rage of the combatant, is at once soothed and satisfied as it
+          watches the wandering of the tendril, and the budding of the flower. …
+          The sculptor who sought for his models among the forest leaves, could
+          not but quickly and deeply feel that complexity need not involve the
+          loss of grace, nor richness that of repose.” The gothic labors are
+          distinct for this naturalism, this realism, built out of “a profound
+          sympathy with the fulness and wealth of the material universe.”
+        </Text>
+        <Text textStyle="paragraph">
+          In the memelore and production of the builders of the decentralized
+          web, we find these same attributes. We find the savage grasp of
+          materiality - yes, even the materiality of transistor charges and
+          logic gates - loved and embraced for all its robustness. When the
+          relationship to the elements isn&apos;t alienated by the distracted
+          and antireal metaphysics of hierarchy, that abundance unfolds, so that
+          the intention of the producer is fused with the intrigue and chance of
+          the material instance. That&apos;s the question- how can we bring the
+          material instance to bear in building, conceptualizing, and
+          manifesting the flamboyantly fractal and utterly irreverent scale
+          invariance of the network, the (anti)thing that touches and ignites
+          the rough whole? In work is an imaginatively sober play.
+        </Text>
+        <Text textStyle="paragraph">
+          Borrowing from the ethos of the builders, we announce The Open
+          Machine, a publication press for the advancement of concepts adequate
+          to the network. We forego authority and professionalism for
+          nonlinearity and free association. We call to task interdisciplinary
+          and transhistorical inputs for the sake of conceptual pandemonium. We
+          study the rhythms and arrows of meme propagation so as to venture
+          farther out onto limbs of recursion and emergence. We work through the
+          abandonment issues of those that have forsaken the lords of enclosure
+          and find new idols, machine elves and mathematical witches of the open
+          past and the long future.
+        </Text>
+        <Text textStyle="paragraph">
+          We create in an atmosphere of savageness and generosity. We view the
+          expanse with the disturbed eye of reality. Atop the flat bazaar, we
+          build an untimely cathedral, a mayhem of free successions that might
+          iterate off into the outer spaces, legibility in non-institutional
+          time.
+        </Text>
+      </Box>
+      <Box>
+        <Text textStyle="paragraph">
+          <Image src="../assets/cathedral_void.png" alt="Cathedral" />
+        </Text>
+      </Box>
+      <Rule />
     </Box>
   </Box>
 )
