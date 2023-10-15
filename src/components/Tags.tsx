e780f66a0ffb9cff3500753b2bbcbaf4ce4fc065
exeunt3 <90937997+exeunt3@users.noreply.github.com>
2023-10-14 23:46:02 -0700

    Update Tags.tsx

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,18 +1,1 @@
-import React from 'react'
-import { Box, Text } from '@chakra-ui/react'
 
-export const Tags = () => (
-  <Box w="100%">
-    <Box maxW="720px" mx="auto" px={12} pb={12}>
-      <Text textStyle="tag">The open machine is ((the earth)), understood as a cybernetic phenomenon.</Text>
-      <Text textStyle="tag">The open machine is an ethos of encounter over ownership.</Text>
-      <Text textStyle="tag">The open machine is a machinic negotiation between intention and indeterminacy.</Text>
-      <Text textStyle="tag">The open machine is a cryptographic gate against the tyranny of the present.</Text>
-      <Text textStyle="tag">The open machine is a stochastic vehicle of emergence and fugitive time.</Text>
-      <Text textStyle="tag">The open machine is a pirate chemistry.</Text>
-      <Text textStyle="tag">The open machine is an archive apophenia.</Text>
-      <Text textStyle="tag">The open machine is a network incantation.</Text>
-      <Text textStyle="tag"><i>The open machine.</i></Text>
-    </Box>
-  </Box>
-)
