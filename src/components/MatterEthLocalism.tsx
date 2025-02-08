a58ff5ce49f2c5e1954a0225a6b41d7457c40a25
Ven Gist <vengist@odyssy.io>
2025-02-08 13:27:18 -0800

    adds two more matters and cleans

---
+++ src/components/MatterEthLocalism.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Box, Button, Flex, Image, Link, Text } from '@chakra-ui/react'
+
+export const MatterEthLocalism = () => (
+  <Box w="100%" p={12} id="matters">
+    <Flex
+      maxW="800px"
+      mx="auto"
+      border="1px solid"
+      borderColor="primary"
+      flexWrap="wrap"
+    >
+      <Flex direction="column" w={['100%', null, '55%']} p={12}>
+        <Text textStyle="h6" mb={6}>
+          The Open Machine #3
+        </Text>
+        <Text as="h2" textStyle="h2">
+          Ethereum Localism
+        </Text>
+        <Text as="h3" textStyle="h3">
+          Grounding the Future of Coordination
+        </Text>
+        <Text mt={6}>Collated by the Open Machine</Text>
+        <Flex gap={6} mt={12}>
+          <Button as={Link} href="#" target="_blank" rel="noopener noreferrer">
+            Coming Soon
+          </Button>
+        </Flex>
+      </Flex>
+      <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        w={['100%', null, '45%']}
+        p={12}
+      >
+        <Image src="/assets/EL_cover-front.jpg" alt="ethereum localism cover" />
+      </Flex>
+    </Flex>
+  </Box>
+)
