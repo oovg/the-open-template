5a38f20d834e6bf189e635bfd3233f9b8970ea9e
exeunt <90937997+exeunt3@users.noreply.github.com>
2025-02-19 21:12:18 -0800

    Update index.ts

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -51,8 +51,8 @@ const overrides = {
   },
   semanticTokens: {
     colors: {
-      primary: { _light: '#282828', _dark: '#fff7ed' },
-      bg: { _light: '#fff7ed', _dark: '#282828' },
+      primary: { _light: '#282828', _dark: '#f0fdfa' },
+      bg: { _light: '#f0fdfa', _dark: '#282828' },
     },
   },
 }
